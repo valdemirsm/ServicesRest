20fbe19c8fa83278cab651f570269696d11e218c
Valdemir Macedo <valdemirsm@gmail.com>
2020-03-10 23:57:33 -0300

    primeiro commit

---
+++ src/main/java/com/valdemir/ServicesRest/ServicesRestApplication.java
@@ -0,0 +1,13 @@
+package com.valdemir.ServicesRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdemir/ServicesRest/ServicesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdemir.ServicesRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
